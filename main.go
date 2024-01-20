4f0874c8fc8a38fbd51889e6e38cdb99e895159c
Johnathan <john.2801.oliver@gmail.com>
2024-01-20 16:01:04 +0000

    Parse h2, h3, h4... && parse paragraphs

--- main.go
+++ main.go
@@ -7,26 +7,6 @@ import (
     "strings"
 )
 
-type TokenKind int
-
-const (
-    TokenUnknown TokenKind = iota
-    TokenH1 
-)
-
-func (kind TokenKind) toString() string {
-    switch kind {
-	case TokenUnknown: return "TokenUnknown" 
-	case TokenH1:      return "TokenH1"
-    }
-    return ""
-}
-
-type Token struct {
-    kind  TokenKind
-    value string
-}
-
 func readFile(filePath string) (string, error) {
     file, err := os.Open(filePath)
     if err != nil {
@@ -53,6 +33,89 @@ func readFile(filePath string) (string, error) {
     return string(buffer), nil
 }
 
+type TokenKind int
+
+const (
+    TokenUnknown TokenKind = iota
+    TokenHeading
+    TokenParagraph
+    TokenCount
+)
+
+func (kind TokenKind) toString() string {
+    if TokenCount > 3 {
+	panic("missing name of new token added")
+    }
+    switch kind {
+	case TokenHeading: return "TokenHeading"
+	case TokenParagraph: return "TokenParagraph"
+    }
+    return "TokenUnknown" 
+}
+
+type Token struct {
+    kind  TokenKind
+    level int
+    value string
+}
+
+func (t *Token) toString() string {
+    return fmt.Sprintf("Token {%s, %d, '%s'}\n", t.kind.toString(), t.level, t.value)
+}
+
+func parseHeading(line string) Token {
+    i := 0
+    for ; i < len(line); i += 1 {
+	if line[i] != '#' {
+	    break
+	}
+    }
+    if i > 6 {
+	return Token{TokenUnknown, 0, ""} // todo: handle heading greater than 6 levels.
+    }
+    return Token{TokenHeading, i, strings.Trim(line[i:], " ")}
+}
+
+func parseParagraph(lines []string) ([]string, Token) {
+    value := ""
+    for len(lines) > 0 {
+	line := lines[0]
+	if len(line) == 0 {
+	    break
+	}
+	if len(value) > 0 {
+	    value += " "
+	}
+	value += line
+	lines = lines[1:]
+    }
+    return lines, Token{TokenParagraph, 0, value}
+}
+
+func parse(source string) []Token {
+    lines := strings.Split(source, "\n")
+    tokens := []Token{}
+
+    for len(lines) > 0 { // todo: factor this for loop to a parseToken function.
+	line := lines[0]
+	token := Token{}
+
+	if len(line) > 0 {
+	    switch line[0] {
+		case '#':
+		    token = parseHeading(line)
+		default:
+		    lines, token = parseParagraph(lines)
+	    }
+
+	}
+
+	lines = lines[1:] // fix: when paragraph eat lines we should not do it here.
+	tokens = append(tokens, token)
+    }
+    return tokens
+}
+
 func main() {
     filePath := "./input.md"
 
@@ -61,18 +124,8 @@ func main() {
 	panic("failed to read file")
     }
 
-    tokens := []Token{}
-    for _, line := range strings.Split(content, "\n") {
-	token := Token{TokenUnknown, line}
-
-	if len(line) > 0 && line[0] == '#' {
-	    token.kind = TokenH1
-	}
-
-	tokens = append(tokens, token)
-    }
-
+    tokens := parse(content)
     for _, token := range tokens {
-	fmt.Printf("Token{%s, '%s'}\n", token.kind.toString(), token.value)
+	fmt.Printf(token.toString())
     }
 }
